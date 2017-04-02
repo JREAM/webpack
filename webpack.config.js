952aa207843f0670bfc4f4587db3669567d11882
Jesse Boyer <imboyus@gmail.com>
2017-04-02 11:23:25 -0400

    airbnb jshint

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,73 @@
-const path = require('path');
+'use strict';
+const path = require('path');  // built into node
 
-module.exports = {
+// utility
+var _ = require('lodash');
+
+// for SASS
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
+]
+
+// main config
+const config = {
 
   // str | obj | arr
   // Where webpack starts exec
-  entry: "./entry.js",
+  entry: [
+    './entry.js'
+  ],
+
+  modules: {
+
+    devtool: 'source-map',
+
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+            loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+            loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+            loader: 'sass-loader' // compiles Sass to CSS
+            // , options: {
+            //   includePaths: ['absolute/path/a', 'absolute/path/b']
+            // }
+        }]
+
+
+    }] // end of 'rules'
+
+  },
 
   // Where compilation/results go
   output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    // Where index file is for site
+    publicPath: '/assets',
+    sourceMapFilename: '[file].map'
+  },
 
-    // must be absolute path
-    // str
-    path: path.resolve(__dirname, "dist"),
-
-    // str
-    filename: "bundle.js",
-
-    // public path relative to index page
-    // str
-    publicPath: "/assets/",
-
-    // filename template of src map location (adding /srcmap/[file].map is ok)
-    // str -- required?
-    sourceMapFilename: "[file].map",
-
-    // Not sure if this is right
-    // devtoolModuleFilenameTemplate: "webpack:///[.node_modules/webpack/bin/webpack"
-
-    // Looks interesting
-    // crossOriginLoading: "use-credentials",
-    // crossOriginLoading: "anonymous",
-    // crossOriginLoading: false
+  plugins: [
+    // For development (Yea?)
+    new ExtractTextPlugin('[name].css')
+  ],
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.sass', '.scss'],
+    root: [path.join(__dirname, './src')]
   }
 
 }
+
+module.exports = config;
